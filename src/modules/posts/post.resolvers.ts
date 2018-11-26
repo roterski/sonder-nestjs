ab3541150ac1898237fbafcfa4bfaa390f562560
Piotr Roterski <pt.roterski@gmail.com>
2018-11-27 00:44:53 +0100

    WIP add comments

--- src/modules/posts/post.resolvers.ts
+++ src/modules/posts/post.resolvers.ts
@@ -1,31 +1,42 @@
 import { ParseIntPipe, UseGuards } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver, Subscription, ResolveProperty } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription, ResolveProperty, Parent } from '@nestjs/graphql';
 import { PubSub } from 'graphql-subscriptions';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Post } from './post.entity';
+import { Comment } from './comment.entity';
 import { GqlAuthGuard } from '../auth/graphql-auth.guard';
 import { PostsService } from './posts.service';
+import { CommentsService } from './comments.service';
 import { CreatePostDto } from './dto/create-post.dto';
 
 const pubSub = new PubSub();
 
 @Resolver('Post')
-@UseGuards(GqlAuthGuard)
+// @UseGuards(GqlAuthGuard)
 export class PostsResolvers {
-  constructor(private readonly postsService: PostsService) { }
+  constructor(
+    private readonly postsService: PostsService,
+    private readonly commentsService: CommentsService
+  ) { }
 
-  @Query('posts')
+  @Query('getPosts')
   getPosts(): Observable<Post[]> {
     return this.postsService.findAll();
   }
 
-  @Query('post')
+  @Query('getPost')
   findOneById(
     @Args('id', ParseIntPipe)
     id: number,
   ): Observable<Post> {
     return this.postsService.findOneById(id);
+  }
+
+  @ResolveProperty()
+  comments(@Parent() post): Observable<Comment[]> {
+    const { id } = post;
+    return this.commentsService.findByPost(id);
   }
 
   @Mutation('createPost')
