4af9ef6c3eaa151e2b4a484e771ffb7c05e81293
Piotr Roterski <pt.roterski@gmail.com>
2019-03-05 20:51:34 +0100

    Fix generated graphql.schema path

--- src/modules/common/graphql/index.ts
+++ src/modules/common/graphql/index.ts
@@ -6,7 +6,7 @@ import * as env from 'env-var';
 export const graphqlConfig: GqlModuleOptions = {
   typePaths: ['./**/**/*.graphql'],
   definitions: {
-    path: join(process.cwd(), 'src/common/graphql/graphql.schema.ts'),
+    path: join(process.cwd(), 'src/modules/common/graphql/graphql.schema.ts'),
     outputAs: 'class',
   },
   debug: env.get('NODE_ENV').required().asString() === 'production' ? false : true,
