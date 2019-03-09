5837829bf45f3659ba4d9d443f1b874d5500e2e6
Piotr Roterski <pt.roterski@gmail.com>
2019-03-09 12:38:53 +0100

    Remove handling graphql from logging.interceptor

--- src/modules/common/interceptors/logging.interceptor.ts
+++ src/modules/common/interceptors/logging.interceptor.ts
@@ -12,7 +12,6 @@ export class LoggingInterceptor implements NestInterceptor {
   ): Observable<any> {
     const now = Date.now();
     const req = _.get(context, 'args[0]');
-    const graphqlReq =  _.get(context, 'args[2].req');
 
     return call$.pipe(tap(() => {
       const elapsed = `+${Date.now() - now} ms`;
@@ -20,9 +19,6 @@ export class LoggingInterceptor implements NestInterceptor {
 
       if (req && req.method && req.path) {
         console.log(timestamp, `[${req.method}] ${req.path}`, req.params, req.query, anonymize()(req.body), elapsed);
-      } else if (graphqlReq) {
-        console.log(timestamp, `[${graphqlReq.method}] ${graphqlReq.originalUrl}`, graphqlReq['body']['variables']);
-        console.log(graphqlReq['body']['query'], elapsed);
       }
     }))
   }
