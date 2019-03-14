f67b12ae9e7b12dd8ab60eaf13a396a4dfdfd307
Piotr Roterski <pt.roterski@gmail.com>
2019-03-14 23:03:01 +0100

    Remove @nestjs/graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { PostsModule } from './modules/posts/posts.module';
